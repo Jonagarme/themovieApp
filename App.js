cea19c8ac1d2daf7f07ad4f9681f70ad3405bcd6
Jonagarme <garciajonathan268@gmail.com>
2020-09-21 11:42:24 -0500

    configurando dark theme

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import {Text, SafeAreaView} from 'react-native';
-import {Provider as PaperProvider} from 'react-native-paper';
-import {Button, Card} from 'react-native-paper';
-import {NavigationContainer} from '@react-navigation/native';
+import {StatusBar} from 'react-native';
+import {
+  Provider as PaperProvider,
+  DarkTheme as DarkThemePaper,
+  DefaultTheme as DefaultThemePaper,
+} from 'react-native-paper';
+import {
+  NavigationContainer,
+  DarkTheme as DarkThemeNavigation,
+  DefaultTheme as DefaultThemeNavigation,
+} from '@react-navigation/native';
 import Navigation from './src/navigation/Navigation';
 
 export default function App() {
+  DefaultThemePaper.colors.primary = '#1ae1f2';
+  DarkThemePaper.colors.primary = '#1ae1f2';
+  DarkThemePaper.colors.accent = '#1ae1f2';
+
+  DarkThemeNavigation.colors.background = '#192734';
+  DarkThemeNavigation.colors.card = '#15212b';
+
   return (
-    <PaperProvider>
-      <NavigationContainer>
+    <PaperProvider theme={DarkThemePaper}>
+      <NavigationContainer theme={DarkThemeNavigation}>
+        <StatusBar barStyle={'light-content'} />
         <Navigation />
       </NavigationContainer>
     </PaperProvider>
--- src/navigation/DrawerContent.js
+++ src/navigation/DrawerContent.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {DrawerContentScrollView} from '@react-navigation/drawer';
 import {Drawer, Switch, TouchableRipple, Text} from 'react-native-paper';
+import {set} from 'react-native-reanimated';
 
 export default function DrawerContent(props) {
   const {navigation} = props;
+  const [active, setActive] = useState('home');
   console.log(props);
+
+  const onChangeScreen = (screen) => {
+    setActive(screen);
+    navigation.navigate(screen);
+  };
+
   return (
     <DrawerContentScrollView>
       <Drawer.Section>
         <Drawer.Item
           label="Inicio"
-          onPress={() => navigation.navigate('news')}
+          active={active === 'home'}
+          onPress={() => onChangeScreen('home')}
+        />
+        <Drawer.Item
+          label="Peliculas Populares"
+          active={active === 'popular'}
+          onPress={() => onChangeScreen('popular')}
+        />
+        <Drawer.Item
+          label="Nuevas Peliculas"
+          active={active === 'news'}
+          onPress={() => onChangeScreen('news')}
         />
       </Drawer.Section>
     </DrawerContentScrollView>
