a366b1d0db8ffb5557c38ecd2d410d4df94dafac
Jonagarme <garciajonathan268@gmail.com>
2020-10-07 21:13:25 -0500

    funcion para buscar peliculas

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, View} from 'react-native';
+import {searchMoviesApi} from '../api/movies';
 
 export default function Search() {
+  const [movies, setMovies] = useState(null);
+
+  useEffect(() => {
+    searchMoviesApi().then((response) => {
+      setMovies(response.results);
+    });
+  }, []);
+
   return (
     <View>
       <Text>Estamos en search</Text>
