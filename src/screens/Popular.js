da893c604ba9d0ab4d7959bbdf3cc3554854b561
Jonagarme <garciajonathan268@gmail.com>
2020-10-07 20:27:18 -0500

    mostrando los datos de las peli populares

--- src/screens/Popular.js
+++ src/screens/Popular.js
@@ -1,14 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, ScrollView, Image} from 'react-native';
-import {Text} from 'react-native-paper';
+import {Text, Title} from 'react-native-paper';
 import {map} from 'lodash';
 import {getPopularMoviesApi} from '../api/movies';
 import {BASE_PATH_IMG} from '../utils/constants';
 import noImage from '../assets/png/default-image.png';
+import {Rating} from 'react-native-ratings';
+import usePreferences from '../hooks/usePreferences';
+import starDark from '../assets/png/starDark.png';
+import starLight from '../assets/png/starLight.png';
 
 export default function Popular(props) {
   const {navigation} = props;
   const [movies, setMovies] = useState(null);
+  const {theme} = usePreferences();
 
   useEffect(() => {
     getPopularMoviesApi(1).then((response) => {
@@ -19,15 +24,15 @@ export default function Popular(props) {
   return (
     <ScrollView>
       {map(movies, (movie, index) => (
-        <Movie key={index} movie={movie} />
+        <Movie key={index} movie={movie} theme={theme} />
       ))}
     </ScrollView>
   );
 }
 
 function Movie(props) {
-  const {movie} = props;
-  const {poster_path} = movie;
+  const {movie, theme} = props;
+  const {poster_path, title, release_date, vote_count, vote_average} = movie;
   return (
     <View style={styles.movie}>
       <View>
@@ -39,8 +44,35 @@ function Movie(props) {
         />
       </View>
       <View>
-        <Text>RIGH</Text>
+        <Title>{title}</Title>
+        <Title>{release_date}</Title>
+        <MovieRating
+          voteCount={vote_count}
+          voteAverage={vote_average}
+          theme={theme}
+        />
       </View>
+    </View>
+  );
+}
+
+function MovieRating(props) {
+  const {theme, voteCount, voteAverage} = props;
+  const media = voteAverage / 2;
+  return (
+    <View style={styles.viewRating}>
+      <Rating
+        type="custom"
+        ratingImage={theme === 'dark' ? starDark : starLight}
+        ratingColor="#ffc205"
+        ratingBackgroundColor={theme === 'dark' ? '#192734' : '#f0f0f0'}
+        startingValue={media}
+        imageSize={20}
+        style={{marginRight: 15}}
+      />
+      <Text style={{fontSize: 12, color: '#8697a5', marginTop: 5}}>
+        {voteCount} votos
+      </Text>
     </View>
   );
 }
